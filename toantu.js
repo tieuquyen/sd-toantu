441862e78c5573edd399893dbb178a978bf5c6f7
tieuquyen <0979128988x@gmail.com>
2019-08-02 13:42:26 +0700

    test

---
+++ toantu.js
@@ -0,0 +1,8 @@
+var inputwidth;
+var inputheight;
+inputwidth=prompt("Enter the width")
+inputheight=prompt("Enter the height")
+var width=parseInt(inputwidth);
+var height=parseInt(inputheight);
+var area=width*height;
+document.write("The area is:" +area);
